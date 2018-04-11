aa276d1f8f7c774c61f109c2117197cbdb7dd567
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-04-11 20:37:27 +0100

    updates js to work to go home

--- script/main.js
+++ script/main.js
@@ -3,25 +3,19 @@ const links = document.querySelectorAll('[data-target]');
 for (var i = 0; i < links.length; i++) {
   links[i].addEventListener('click', function(e) {
 
-
-    // this should be the id of where we are going next
-    var nextPageAttr = this.getAttribute('data-target');
-    console.log('what are we moving to: ' + nextPageAttr)
-
-    // this should be the page we are shutting down (current page)
+    // this is the section currently showing (current section)
     var currSection = document.querySelector('section.show');
-    console.log('where are we now: ' + currSection)
 
-    // this should be where we go next
+    // this is the 'data-target' attribute (where the button should lead to)
+    var nextPageAttr = this.getAttribute('data-target');
+
+    // this uses the name of the next page (nextPageAttr)
+    // to find the page we want to show
     var nextPage = document.querySelector('[data-name='+ nextPageAttr +']');
-    console.log('what page are we going to show: ' + nextPage)
-    nextPage.classList.add('show');
 
-
-    // this should be the id of the page we are shutting down (current page)
-    var currAttr = currSection.getAttribute('data-name');
-    console.log('what page should we shut down: ' + currAttr)
+    // this hides the previous page…
     currSection.classList.remove('show');
-
+    // …and shows the next one
+    nextPage.classList.add('show');
   });
 }
