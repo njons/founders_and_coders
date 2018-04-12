30ec266e7450b2dacc5065a4aef8761a071da0ca
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2018-04-12 18:01:56 +0100

    moved some files to the right place

--- script/main.js
+++ script/main.js
@@ -3,19 +3,21 @@ const links = document.querySelectorAll('[data-target]');
 for (var i = 0; i < links.length; i++) {
   links[i].addEventListener('click', function(e) {
 
-    // this is the section currently showing (current section)
-    var currSection = document.querySelector('section.show');
+    // this is the section currently showing
+    //(current section)
+    const currSection = document.querySelector('section.show');
 
-    // this is the 'data-target' attribute (where the button should lead to)
-    var nextPageAttr = this.getAttribute('data-target');
+    // this is the 'data-target' attribute
+    //(where the button should lead to)
+    const nextSectionAttr = this.getAttribute('data-target');
 
     // this uses the name of the next page (nextPageAttr)
     // to find the page we want to show
-    var nextPage = document.querySelector('[data-name='+ nextPageAttr +']');
+    const nextSection = document.querySelector('[data-name='+ nextSectionAttr +']');
 
     // this hides the previous page…
     currSection.classList.remove('show');
     // …and shows the next one
-    nextPage.classList.add('show');
+    nextSection.classList.add('show');
   });
 }
